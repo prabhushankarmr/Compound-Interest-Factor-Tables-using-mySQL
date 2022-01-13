f80c3924dd671cc56cb1aac813e93f7a29fef62c
prabhushankarmr <64687895+prabhushankarmr@users.noreply.github.com>
2022-01-13 20:15:44 +0530

    Add files via upload

---
+++ DEMO3.sql
@@ -0,0 +1,4 @@
+INSERT INTO `COMPOUND_INTEREST_TABLES`.`2.5` (`n`, `F/P`, `P/F`, `A/F`, `A/P`, `F/A`, `P/A`, `A/G`, `P/G`) 
+VALUES ('1', '1.025', '.9756', '1.0000', '1.0250', '1.000', '0.976', '0', '0');
+
+SELECT * FROM `COMPOUND_INTEREST_TABLES`.`2.5`;
